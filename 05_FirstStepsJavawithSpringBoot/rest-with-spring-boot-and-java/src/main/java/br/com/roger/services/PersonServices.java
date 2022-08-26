bce730177b7121e4a6e4186e022953462b22f918
Roger Chaves <roger.chaves.rs2@gmail.com>
2022-08-26 09:50:38 -0300

    Utilizando POST,PUT,DELETE

---
+++ 05_FirstStepsJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/roger/services/PersonServices.java
@@ -0,0 +1,65 @@
+package br.com.roger.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.roger.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		logger.info("Finding all people!");
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+	
+
+	public Person findById(String id) {
+		
+		logger.info("Finding one Person!");
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Roger");
+		person.setLastName("Chaves");
+		person.setAddress("Porto Alegre - Rio grande do sul - Brasil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one Person!");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one Person!");
+		return person;
+	}
+	
+	public void  delete(String id) {
+		logger.info("Delete one Person!");
+		}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name; " + i);
+		person.setLastName("Last name; " + i);
+		person.setAddress("Adress in Brasil; " + i);
+		person.setGender("Male");
+		return person;
+	}
+}
