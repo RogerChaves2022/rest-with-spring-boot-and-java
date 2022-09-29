d8f81eb418dee5fb613c7bd679a9e4828154237d
Roger Chaves <roger.chaves.rs2@gmail.com>
2022-09-29 08:57:47 -0300

    Finalizando seção incluindo token e refresh token, testes com postman

---
+++ 16_FirstStepsJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/roger/mapper/custom/PersonMapper.java
@@ -0,0 +1,37 @@
+package br.com.roger.mapper.custom;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import br.com.roger.data.vo.v2.PersonVOV2;
+import br.com.roger.model.Person;
+
+@Service
+public class PersonMapper {
+
+	public PersonVOV2 convertEntityToVo(Person person) {
+		PersonVOV2 vo = new PersonVOV2();
+		vo.setId(person.getId());
+		vo.setFirstName(person.getFirstName());
+		vo.setLastName(person.getLastName());
+		vo.setAddress(person.getAddress());
+		vo.setGender(person.getGender());
+		vo.setBirthDay(new Date());
+		
+		return vo;
+	}
+	
+	public Person convertVoToEntity(PersonVOV2 person) {
+		Person entity = new Person();
+		entity.setId(person.getId());
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		//vo.setBirthDay(new Date());
+		
+		return entity;
+	}
+	
+}
